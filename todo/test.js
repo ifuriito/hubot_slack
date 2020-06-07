93b259242d7c94579fd85eb94a90375ac32e8f3a
ifuriito <y.ota.rkt@gmail.com>
2020-06-07 17:44:47 +0900

    moduleの追加

--- todo/test.js
+++ todo/test.js
@@ -7,4 +7,15 @@ todo.todo('ノートを買う');
 todo.todo('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
+// done と donelist のテスト
+todo.done('鉛筆を買う');
+assert.deepEqual(todo.list(), ['ノートを買う']);
+assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
+
+// del のテスト
+todo.del('ノートを買う');
+todo.del('鉛筆を買う');
+assert.deepEqual(todo.list(), []);
+assert.deepEqual(todo.donelist(), []);
+
 console.log('テストが正常に完了しました');
